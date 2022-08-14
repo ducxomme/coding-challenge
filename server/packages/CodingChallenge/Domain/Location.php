37bac96ef050c88183e993de520c28b69bf93952
ducxomme <ducxomme@gmail.com>
2022-08-14 22:05:10 +0700

    [update] GG APIを利用して、軽度緯度取得できるようにする

--- server/packages/CodingChallenge/Domain/Location.php
+++ server/packages/CodingChallenge/Domain/Location.php
@@ -4,9 +4,59 @@ declare(strict_types=1);
 namespace CodingChallenge\Domain;
 
 /**
- *
+ * Class Location
+ * @package CodingChallenge\Domain
  */
 final class Location
 {
+    /**
+     * @var Latitude|null
+     */
+    private ?Latitude $latitude;
 
+    /**
+     * @var Longitude|null
+     */
+    private ?Longitude $longitude;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->latitude = null;
+        $this->longitude = null;
+    }
+
+    /**
+     * @return Latitude|null
+     */
+    public function getLatitude(): ?Latitude
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param Latitude|null $latitude
+     */
+    public function setLatitude(?Latitude $latitude): void
+    {
+        $this->latitude = $latitude;
+    }
+
+    /**
+     * @return Longitude|null
+     */
+    public function getLongitude(): ?Longitude
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param Longitude|null $longitude
+     */
+    public function setLongitude(?Longitude $longitude): void
+    {
+        $this->longitude = $longitude;
+    }
 }
--- server/packages/CodingChallenge/Infrastructure/GoogleRepository.php
+++ server/packages/CodingChallenge/Infrastructure/GoogleRepository.php
@@ -3,19 +3,46 @@ declare(strict_types=1);
 
 namespace CodingChallenge\Infrastructure;
 
+use CodingChallenge\Domain\Latitude;
 use CodingChallenge\Domain\Location;
 use CodingChallenge\Domain\IGoogleRepository;
+use CodingChallenge\Domain\Longitude;
+use CodingChallenge\Infrastructure\Common\ApiClient;
+use Exception;
 
 /**
- *
+ * Class GoogleRepository
+ * @package CodingChallenge\Infrastructure
  */
 final class GoogleRepository implements IGoogleRepository
 {
     /**
      * @inheritDoc
+     * @throws Exception
      */
     public function getLocation(string $address): Location
     {
-        return new Location();
+        $baseUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+        $key = env('GOOGLE_API_KEY');
+
+        $address = urlencode($address);
+
+        $params = "address=$address&key=$key";
+
+        $location = null;
+        $result = ApiClient::get("$baseUrl?{$params}");
+
+        if (!empty($result['results'])) {
+            if (!is_null($result['results'][0]['geometry']['location'])) {
+                $location = new Location();
+
+                $locationResult = $result['results'][0]['geometry']['location'];
+
+                $location->setLatitude(new Latitude((float)$locationResult['lat']));
+                $location->setLongitude(new Longitude((float)$locationResult['lng']));
+            }
+        }
+
+        return $location;
     }
 }
